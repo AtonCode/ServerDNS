9315a29a3b408df5338b7f8200e7781f3f1f12fb
AtonCode <49330231+AtonCode@users.noreply.github.com>
2021-04-25 00:04:27 -0500

    AtonCodeMac

--- main.py
+++ main.py
@@ -58,9 +58,9 @@ def cacheWrite(queryRespondDNSFriend):
 
     try:
         flow = open('zones/cache.txt','a')
-        flow.write(str(codecs.decode(domainName,'UTF-8')))
+        flow.write(str(codecs.decode(domainName)))
         flow.write('\n')
-        flow.write(str(codecs.decode(domineType,'UTF-8')))
+        flow.write(str(codecs.decode(domineType)))
         flow.write('\n')
         flow.write(str(queryRespondDNSFriend))
         flow.write('\n')
